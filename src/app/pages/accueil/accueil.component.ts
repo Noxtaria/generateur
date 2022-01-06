819e5de997dd2c05c8f43ed335f75eaa30cb466d
Kevin Audacioza <kevin@audacioza.com>
2022-01-06 13:23:30 +0100

    Trad + test Laurent

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -8,15 +8,12 @@ import { Checkbox } from 'src/app/models/Checkbox';
 })
 export class AccueilComponent implements OnInit {
 
-  checkbox: Checkbox[] = [];
+  checkboxes: Checkbox[] = [];
+  budget: string;
 
-  constructor(
-
-  ) { 
-  }
+  constructor() { }
 
   ngOnInit(): void {
-
     this.generateCheckboxFormat();
     this.afficherPrix();
   }
@@ -24,21 +21,25 @@ export class AccueilComponent implements OnInit {
   //generateCheckbox"Format"  Format = à modifier selon la demande
   private generateCheckboxFormat = () => {
 
-    // Genere les valeur des input checkbox
-    this.checkbox=[
+    // Genere les valeurs des inputs checkbox
+    this.checkboxes=[
       {id:1, name:"Format Rapide", prix: "", isselected:false},
       {id:2, name:"Format Standard", prix: "2000 €", isselected:false},
       {id:3, name:"Format Long", prix: "5000 €" , isselected:false}
     ]
   }
 
-
   public afficherPrix = () => {
-
+    console.log("afficherPrix");
+    
   }
 
   // Permet de savoir ce qui est selectionné
   public onChange = () => {
-    console.log(this.checkbox);
+    console.log(this.checkboxes);
+  }
+
+  getCheckboxSelected = () => {
+
   }
 }
