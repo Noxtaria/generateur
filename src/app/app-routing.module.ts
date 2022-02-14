299b0605785dde771ca1da7698811dbefee95b8b
Kevin Audacioza <kevin@audacioza.com>
2022-02-14 13:07:52 +0100

    Mise en place de l'HTML / CSS Accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './pages/accueil/accueil.component';
+import { MotionComponent } from './pages/motion/motion.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   { path: "", component: AccueilComponent },
+  { path: "motion", component: MotionComponent},
   { path: "**", component: NotFoundComponent}
 ];
 
